3b6cae3dced6040fdb28a0829d46da01b9ae2a55
Joey <theagentjoey@gmail.com>
2026-05-17 05:11:31 +0800

    feat(web): SessionList sidebar + ConnectionBadge

---
+++ web/src/lib/components/ConnectionBadge.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { connection } from '../stores/connection.js'
+</script>
+
+<span class="badge">
+  {#if $connection === 'connected'}
+    🟢 connected
+  {:else if $connection === 'reconnecting'}
+    🟡 reconnecting…
+  {:else}
+    🔴 offline
+  {/if}
+</span>
+
+<style>
+  .badge {
+    font-size: 0.8em;
+    color: #888;
+  }
+</style>
